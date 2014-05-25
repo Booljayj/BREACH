455a3af754fe32fdec7f151b828e5097e7832eca
Justin <booljayj@gmail.com>
2014-05-24 19:16:30 -0800

    In progress with door upgrades

--- Assets/Scripts/DoorAnimationListener.cs
+++ Assets/Scripts/DoorAnimationListener.cs
@@ -2,12 +2,29 @@
 using System.Collections;
 
 public class DoorAnimationListener : MonoBehaviour {
+	public Messenger message;
+
 	private Door door;
 	private Animator animator;
 
 	public void Start() {
+		if (message == null) {
+			enabled = false;
+			return;
+		}
+
 		door = transform.parent.GetComponent<Door>();
 		animator = GetComponent<Animator>();
+
+	}
+
+	public void ProcessDoorState(DoorStateMessage m) {
+		AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
+		if (m == DoorStateMessage.Open) {
+			animator.CrossFade("OpenState", .1f, 0, 1f-state.normalizedTime);
+		} else if (m == DoorStateMessage.Close) {
+			animator.CrossFade("CloseState", .1f, 0, 1f-state.normalizedTime);
+		}
 	}
 
 	//called at the beginning of the Unsealing animation
--- Assets/Standard Assets/Scripts/Messenger.cs
+++ Assets/Standard Assets/Scripts/Messenger.cs
@@ -19,7 +19,9 @@ public class Messenger : MonoBehaviour {
 	/// <typeparam name="T">The type of message.</typeparam>
 	/// <param name="message">The message.</param>
 	public void Send<T>(T message) {
+		#if DEBUG
 		if (debug) Debug.Log(string.Format("Send ({0}):{1}", typeof(T).ToString(), message.ToString()));
+		#endif
 
 		if (actions.ContainsKey(typeof(T))) {
 			((Action<T>)actions[typeof(T)])(message);
@@ -33,7 +35,9 @@ public class Messenger : MonoBehaviour {
 	/// <param name="message">The message.</param>
 	/// <param name="callback">The callback to be executed when the message receive function has completed</param>
 	public IAsyncResult SendAsync<T>(T message, AsyncCallback callback = null) {
+		#if DEBUG
 		if (debug) Debug.Log(string.Format("Send Async ({0}):{1}", typeof(T).ToString(), message.ToString()));
+		#endif
 
 		if (actions.ContainsKey(typeof(T))) {
 			return ((Action<T>)actions[typeof(T)]).BeginInvoke(message, callback, null);
@@ -49,7 +53,9 @@ public class Messenger : MonoBehaviour {
 	/// <typeparam name="R">The return type of the request.</typeparam>
 	/// <returns>The result of the request.</returns>
 	public R Request<T, R>(T parameter) {
+		#if DEBUG
 		if (debug) Debug.Log(string.Format("Request ({0}):{2} -> ({1})", typeof(T).ToString(), typeof(R).ToString(), parameter.ToString()));
+		#endif
 
 		if (functions.ContainsKey(typeof(T))) {
 			var function = functions[typeof(T)] as Func<T, R>;
@@ -69,7 +75,9 @@ public class Messenger : MonoBehaviour {
 	/// <param name="callback">The callback to be executed when the request finishes.</param>
 	/// <returns>An IAsyncResult for the asynchronous operation.</returns>
 	public IAsyncResult RequestAsync<T, R>(T parameter, AsyncCallback callback = null) {
+		#if DEBUG
 		if (debug) Debug.Log(string.Format("Request Async ({0}):{2} -> ({1})", typeof(T).ToString(), typeof(R).ToString(), parameter.ToString()));
+		#endif
 
 		if (functions.ContainsKey(typeof(T))) {
 			var function = functions[typeof(T)] as Func<T, R>;
@@ -88,7 +96,9 @@ public class Messenger : MonoBehaviour {
 	/// <typeparam name="T">Type of message to receive</typeparam>
 	/// <param name="request">The function that fills the request</param>
 	public void Register<T, R>(Func<T, R> request) {
+		#if DEBUG
 		if (debug) Debug.Log(string.Format("Registered Request ({0}) -> ({1})", typeof(T).ToString(), typeof(R).ToString()));
+		#endif
 
 		if (functions.ContainsKey(typeof(T))) {
 			functions[typeof(T)] = Delegate.Combine(functions[typeof(T)], request);
@@ -104,7 +114,9 @@ public class Messenger : MonoBehaviour {
 	/// <typeparam name="T">Type of message to receive</typeparam>
 	/// <param name="action">The action that happens when the message is received.</param>
 	public void Register<T>(Action<T> action) {
+		#if DEBUG
 		if (debug) Debug.Log(string.Format("Registered ({0})", typeof(T).ToString()));
+		#endif
 
 		if (actions.ContainsKey(typeof(T))) {
 			actions[typeof(T)] = (Action<T>)Delegate.Combine(actions[typeof(T)], action);
@@ -121,7 +133,9 @@ public class Messenger : MonoBehaviour {
 	/// <typeparam name="R">The return type of the request.</typeparam>
 	/// <param name="request">The request to unregister.</param>
 	public void Unregister<T,R>(Func<T,R> request) {
+		#if DEBUG
 		if (debug) Debug.Log(string.Format("Unregistered Request ({0}) -> ({1})", typeof(T).ToString(), typeof(R).ToString()));
+		#endif
 
 		if(functions.ContainsKey(typeof(T))) {
 			functions[typeof(T)] = Delegate.Remove(functions[typeof(T)], request);
@@ -134,7 +148,9 @@ public class Messenger : MonoBehaviour {
 	/// <typeparam name="T">The type of message.</typeparam>
 	/// <param name="action">The action to unregister.</param>
 	public void Unregister<T>(Action<T> action) {
+		#if DEBUG
 		if (debug) Debug.Log(string.Format("Unregistered ({0})", typeof(T).ToString()));
+		#endif
 
 		if(actions.ContainsKey(typeof(T))) {
 			actions[typeof(T)] = (Action<T>)Delegate.Remove(actions[typeof(T)], action);
